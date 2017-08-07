d53373e3a443867fcdc7c9c195342a0643621433
Chitanda60 <freeyinyutian@gmail.com>
2017-08-07 20:08:48 +0800

    日常提交

---
+++ WebSocketWithIOServer.js
@@ -0,0 +1,33 @@
+
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(3002);
+
+function handler (req, res) {
+	fs.readFile(__dirname + '/index.html',
+
+	function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+
+		res.writeHead(200);
+		res.end(data);
+	});
+}
+
+io.on('connection', function (socket) {	
+	socket.emit('achievement_finish', {
+		achievementId: "12345678"
+	});
+});
+
+
+
+
+
+
+
